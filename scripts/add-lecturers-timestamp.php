3d03c6d97f829f5a1f5989e1283e3741a5c577a1
Mykhailo Balakhon <9mohapx9@gmail.com>
2025-03-18 15:43:40 +0100

    Add add-lecturers-timestamp.php

---
+++ scripts/add-lecturers-timestamp.php
@@ -0,0 +1,23 @@
+<?php
+$link = mysqli_connect("localhost", "root", "", "", 3306);
+if ($link) {
+    echo '<p>Connection to server was established</p>';
+} else {
+    echo "<p style='color: red'>Connection to server was NOT established</p>";
+}
+
+$db = "lab1_db";
+$select_db = mysqli_select_db($link, $db);
+if ($select_db) {
+    echo "<p>Database $db was selected</p>";
+} else {
+    echo "<p style='color: red'>Database $db was NOT selected</p>";
+}
+
+$query = "ALTER TABLE lecturers ADD COLUMN created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP";
+$create_table = mysqli_query($link, $query);
+if ($create_table) {
+    echo '<p>Table lecturers was updated</p>';
+} else {
+    echo "<p style='color: red'>Table was NOT updated</p>";
+}
